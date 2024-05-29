1fbecf2f773ec3a2fee1c1c6d1e28fe680e897c3
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2024-05-29 11:37:06 +0200

    run_worker: Stop worker nicely

--- run_worker.py
+++ run_worker.py
@@ -29,7 +29,11 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        print("Worker started")
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Worker stopped")
 
 
 # @@@SNIPEND
